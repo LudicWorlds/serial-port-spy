c53a5eca50bc51e3cf1565a33764e629d3b7fc82
Milan Prucha <ludicworlds@protonmail.com>
2025-05-26 11:24:52 +0200

    Added app Icon.

--- SerialPortSpy/MainWindow.xaml.cs
+++ SerialPortSpy/MainWindow.xaml.cs
@@ -1,27 +1,17 @@
 ﻿/*
  * Serial Port Spy 
  * ---------------
- * by Milan Prucha
  * 
- * Website: www.milanprucha.com
- * GitHub:  github.com/prucha
+ * Website: https://ludicworlds.com
+ * GitHub:  https://github.com/LudicWorlds
  */
 
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO.Ports;
 using System.Diagnostics;
 using System.Windows.Threading;
